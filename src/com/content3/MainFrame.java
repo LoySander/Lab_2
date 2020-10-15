4f26486d59d5d38c5bf9fa5e097370ff11a70bd6
Stanislaw <val_zur@mail.ru>
2020-10-15 22:02:44 +0300

    progress 70-80%

--- src/com/content3/MainFrame.java
+++ src/com/content3/MainFrame.java
@@ -25,10 +25,16 @@ public class MainFrame extends JFrame {
     private JTextField textFieldX;
     private JTextField textFieldY;
     private JTextField textFieldZ;
+    private double mem1,mem2,mem3;
+    private double sum;
     private ButtonGroup radioButtons = new ButtonGroup();
     private Box hboxFormulaType = Box.createHorizontalBox();
     int formulaId= 1;
+    private JTextField memoryTextField;
+    private int memoryId= 1;
     double result = 0;
+    private ButtonGroup radioMemoryButtons = new ButtonGroup();
+    private Box hboxMemoryType = Box.createHorizontalBox();
     public double calculate1 (double x, double y,double z ){
         return x+y+z; // например
     }
@@ -47,6 +53,26 @@ public class MainFrame extends JFrame {
         radioButtons.add(button);
         hboxFormulaType.add(button); // Добавить радио-кнопку в контейнер
         // Для этого ссылка на контейнер сделана полем данных класса
+    }
+    private void addMemoryRadioButton (String buttonName, int memoryId)	{         // радиокнопки для памяти
+        JRadioButton button = new JRadioButton(buttonName);
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event)	{
+                MainFrame.this.memoryId = memoryId;
+                if (memoryId == 1)	{
+                    memoryTextField.setText(toString().format("%-10.3f%n", (mem1)));
+                }
+                if (memoryId == 2){
+                    memoryTextField.setText(toString().format("%-10.3f%n", (mem2)));
+                }
+                if (memoryId == 3){
+                    memoryTextField.setText(toString().format("%-10.3f%n", (mem3)));
+                }
+            }
+        });
+
+        radioMemoryButtons.add(button);
+        hboxMemoryType.add(button);
     }
     public MainFrame(){
         super("Вычисление формулы");
@@ -103,6 +129,7 @@ public class MainFrame extends JFrame {
         hboxResult.add(Box.createHorizontalGlue());
         // Задать рамку для контейнера
         hboxResult.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+        Box actions=Box.createHorizontalBox(); // область действий
         // Создать кнопку «Вычислить»
         JButton buttonCalculation= new JButton("Вычислить");
         // Определить и зарегистрировать обработчик нажатия на кнопку
@@ -128,6 +155,7 @@ public class MainFrame extends JFrame {
                 }
             }
         });
+
         // Создать кнопку «Очистить поля»
         JButton buttonReset= new JButton("Очистить поля");
         // Определить и зарегистрировать обработчик нажатия на кнопку
@@ -139,30 +167,89 @@ public class MainFrame extends JFrame {
                 textFieldResult.setText("0");
             }
         });
-        // Создать коробку с горизонтальной укладкой
-        Box hboxButtons = Box.createHorizontalBox();
-        // Добавить «клей» C4-H1 с левой стороны
-        hboxButtons.add(Box.createHorizontalGlue());
-        // Добавить кнопку «Вычислить» в компоновку
-        hboxButtons.add(buttonCalculation);
-        // Добавить распоркув 30 пикселов C4-H2 между кнопками
-        hboxButtons.add(Box.createHorizontalStrut(30));
-        // Добавить кнопку «Очистить поля» в компоновку
-        hboxButtons.add(buttonReset);
-        // Добавить «клей» C4-H3 с правой стороны
-        hboxButtons.add(Box.createHorizontalGlue());
-        hboxButtons.setBorder(BorderFactory.createLineBorder(Color.GREEN));
-        // Создать контейнер «коробка с вертикальной укладкой»
+        actions.add(Box.createHorizontalGlue());
+        actions.add(buttonCalculation);
+        actions.add(Box.createHorizontalStrut(10));
+        actions.add(buttonReset);
+        actions.add(Box.createHorizontalGlue());
+        hboxMemoryType.add(Box.createHorizontalGlue());
+
+        addMemoryRadioButton("Память 1",1);
+        addMemoryRadioButton("Память 2",2);
+        addMemoryRadioButton("Память 3",3);
+        radioMemoryButtons.setSelected(radioMemoryButtons.getElements().nextElement().getModel(), true);
+        hboxMemoryType.add(Box.createHorizontalGlue());
+
+
+        Box memory_result=Box.createHorizontalBox();
+        memory_result.add(Box.createHorizontalGlue());
+        JButton MC=new JButton("MC");
+        MC.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ev) {
+                if (memoryId == 1){
+                    mem1 = 0;
+                }
+                if (memoryId == 2)	{
+                    mem2 = 0;
+                }
+                if (memoryId == 3)	{
+                    mem3 = 0;
+                }
+                memoryTextField.setText("0");
+            }
+        });
+        JButton M_plus=new JButton("M+");
+        M_plus.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent ev) {
+                try{
+                    Double result = Double.parseDouble(textFieldResult.getText());
+
+                    if (memoryId == 1) 	{
+                        mem1 += result;
+                        memoryTextField.setText(toString().format("%-10.3f%n", (mem1)));
+                    }
+                    if (memoryId == 2)	{
+                        mem2 += result;
+                        memoryTextField.setText(toString().format("%-10.3f%n", (mem2)));
+                    }
+                    if (memoryId == 3)	{
+                        mem3 += result;
+                        memoryTextField.setText(toString().format("%-10.3f%n", (mem3)));
+                    }
+
+                }catch (NumberFormatException ex)
+                { JOptionPane.showMessageDialog(MainFrame.this,
+                        "Ошибка в формате записи числа с плавающей точкой", "" +
+                                "Ошибочный формат числа", JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        });
+        memoryTextField = new JTextField("0",10);
+        memoryTextField.setMaximumSize(memoryTextField.getPreferredSize());
+        memory_result.add(MC);
+        memory_result.add(Box.createHorizontalStrut(10));
+        memory_result.add(memoryTextField);
+        memory_result.add(Box.createHorizontalStrut(10));
+        memory_result.add(M_plus);
+        memory_result.add(Box.createHorizontalGlue());
+
         Box contentBox= Box.createVerticalBox();
-        // Добавить «клей»V1 сверху
         contentBox.add(Box.createVerticalGlue());
         contentBox.add(hboxFormulaType);
         // Добавить контейнер с переменными
         contentBox.add(hboxVariables);
+        contentBox.add(Box.createVerticalGlue());
         // Добавить контейнер с результатом вычислений
         contentBox.add(hboxResult);
+        contentBox.add(Box.createVerticalGlue());
         // Добавить контейнер с кнопками
-        contentBox.add(hboxButtons);
+        contentBox.add(actions);
+        contentBox.add(Box.createVerticalGlue());
+        contentBox.add(hboxMemoryType);
+        contentBox.add(Box.createVerticalGlue());
+        contentBox.add(memory_result);
         // Добавить «клей»V2 снизу
         contentBox.add(Box.createVerticalGlue());
         // Установить «вертикальную коробку» в область содержания главного окна
